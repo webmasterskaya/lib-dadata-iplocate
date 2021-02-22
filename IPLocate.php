53eb711f89b5fbf9c7bf981ec403c9d287b956d5
Artem Vasilev <kern.usr@gmail.com>
2021-02-22 21:22:03 +0300

    Возврат в разработку

--- IPLocate.php
+++ IPLocate.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * @package    Joomla.Library - DaData\IPLocate
- * @version    1.0.0
+ * @version    __DEPLOYMENT_VERSION__
  * @author     Artem Vasilev - webmasterskaya.xyz
  * @copyright  Copyright (c) 2018 - 2021 Webmasterskaya. All rights reserved.
  * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
